a4d8f8e2047dbb7c2aceec714ae7f440b74cec86
wonion <1216963622@qq.com>
2022-07-27 23:57:35 +0800

    init

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>This is wonion's blog</h1>
+        );
+    }
+}
+ 
+export default App;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <React.Fragment>
+                <div>
+                    
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default Header;
